8c2e7d881cdbb9a0b63ead6e4539e9980890c2d9
NkxxkN <kevin.antoine@protonmail.com>
2020-12-14 16:30:47 +0100

    use nextjS supertokens-node helper

--- pages/api/auth/[[...path]].js
+++ pages/api/auth/[[...path]].js
@@ -1,5 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import SuperTokens from "supertokens-node";
+import superTokensMiddleware from "supertokens-node/nextjs";
 import Session from "supertokens-node/recipe/session";
 import EmailPassword from "supertokens-node/recipe/emailpassword";
 import Cors from 'cors';
@@ -10,23 +11,21 @@ const websitePort = process.env.APP_PORT || 3000;
 const websiteDomain = process.env.APP_URL || `http://localhost:${websitePort}`
 const apiBasePath = "/api/auth/";
 
-try {
-    SuperTokens.init({
-        supertokens: {
-            connectionURI: "https://try.supertokens.io", // Replace with your SuperTokens core instance. See https://supertokens.io/docs/emailpassword/quick-setup/supertokens-core/overview
-        },
-        appInfo: {
-            appName: "SuperTokens Demo App",
-            apiDomain,
-            apiBasePath,
-            websiteDomain
-        },
-        recipeList: [
-            EmailPassword.init(),
-            Session.init()
-        ]
-    });
-} catch (e) {}
+SuperTokens.init({
+    supertokens: {
+        connectionURI: "https://try.supertokens.io", // Replace with your SuperTokens core instance. See https://supertokens.io/docs/emailpassword/quick-setup/supertokens-core/overview
+    },
+    appInfo: {
+        appName: "SuperTokens Demo App",
+        apiDomain,
+        apiBasePath,
+        websiteDomain
+    },
+    recipeList: [
+        EmailPassword.init(),
+        Session.init()
+    ]
+});
 
 const cors = Cors({
     origin: websiteDomain,
@@ -47,49 +46,8 @@ function runCORSMiddleware(req, res, fn) {
     })
 }
 
-function runSuperTokensMiddleware(req, res) {
-    const reqProxy = new Proxy(req, {
-        get(target, name) {
-            if (name === `originalUrl`) {
-                return target.url
-            }
-
-            return target[name]
-        }
-    });
-
-    const resProxy = new Proxy(res, {
-        get(target, name) {
-            if (name === `header`) {
-                return target.setHeader
-            }
-
-            return target[name]
-        }
-    });
-
-    return new Promise((resolve, reject) => {
-        SuperTokens.middleware()(reqProxy, resProxy, async (result) => {
-            if (result instanceof Error) {
-                return reject(result)
-            }
-
-            if (result !== undefined) {
-                SuperTokens.errorHandler()(result, req, res, (errorHandlerResult) => {
-                    if (result instanceof Error) {
-                        return reject(result)
-                    }
-
-                    resolve(errorHandlerResult);
-                });
-            };
-
-            return resolve(result);
-        });
-    })
-}
 
 export default async (req, res) => {
     await runCORSMiddleware(req, res, cors)
-    await runSuperTokensMiddleware(req, res);
+    await superTokensMiddleware(req, res);
 }
